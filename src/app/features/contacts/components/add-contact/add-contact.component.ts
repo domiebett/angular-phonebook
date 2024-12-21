50bd63cfd3e798a601b92b3800e746adac6f5ebe
Dominic Bett <dbett49@gmail.com>
2024-12-21 14:06:41 +0300

    task: add form validation

--- src/app/features/contacts/components/add-contact/add-contact.component.ts
+++ src/app/features/contacts/components/add-contact/add-contact.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-add-contact',
   standalone: true,
   imports: [FontAwesomeModule, ContactFormComponent, RouterModule],
   templateUrl: './add-contact.component.html',
-  styleUrl: './add-contact.component.scss'
+  styleUrl: './add-contact.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddContactComponent {
   faArrowLeft = faArrowLeft;
--- src/app/features/contacts/components/contact-form/contact-form.component.ts
+++ src/app/features/contacts/components/contact-form/contact-form.component.ts
@@ -1,13 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { IContact } from '../../models/contacts';
 import { ReactiveFormsModule, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactFormComponent {
   @Input() contact: IContact | {} = {};
@@ -20,19 +22,37 @@ export class ContactFormComponent {
     this.form = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.email]],
       address: [''],
       favorite: [false],
       group: [''],
-      phone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]]
+      phone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
     });
   }
 
   handleSubmit() {
     if (this.form.invalid) {
       console.error('Form is invalid');
+      this.form.markAllAsTouched();
       return;
     }
     this.onSubmit.emit(this.form.value);
+    this.form.reset();
+  }
+
+  getControlClass(controlName: string): string {
+    const control = this.form.get(controlName);
+    return control?.invalid && control?.touched ? 'input-error' : '';
+  }
+
+  controlHasError(controlName: string, error?: string) {
+    const control = this.form.get(controlName);
+    // check if the control has error. if error is provided, then check for the specific error,
+    // otherwise only check if it is invalid.
+    return (
+      control?.invalid &&
+      control.touched &&
+      (error ? control.hasError(error) : true)
+    );
   }
 }
